37aa99ff3174d13c4d3dd96210f1aa6b54348278
miku <lighttools9@gmail.com>
2025-11-29 14:20:12 +0700

    updated crud_test file

--- crud_test.go
+++ crud_test.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"database/sql"
-	"fmt"
 	"log"
-	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
 type User struct {
-	ID 		int
-	Name	string 
-	Email	string  
+	ID    int
+	Name  string
+	Email string
+}
+
+func main() {
+	db, err := sql.Open("mysql", "root:@tcp(127.0.0.1:3306)/testdb")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	// Create table
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS users (
+			id INT AUTO_INCREMENT PRIMARY KEY, 
+			name VARCHAR(100) NOT NULL,
+			email VARCHAR(100) NOT NULL
+		)
+	`)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
 }
